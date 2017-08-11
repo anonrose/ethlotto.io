6619937592f3eff5fd242b215332d4d181993eee
Kyle Rose <kyle.rose@getg5.com>
2017-08-11 01:19:40 -0700

    remove pool add lottery completed

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.4.11;
 contract Lottery {
 
   address public admin;
-  address public lotteryPool;
 
   uint public lotteryStart;
   uint public lotteryTime;
@@ -16,13 +15,15 @@ contract Lottery {
   event LotteryCreated(uint lotteryStart, uint lotteryTime);
   event LotteryDeleted();
 
+  event LotteryCompleted();
+
   Lottery(address _lotteryPool) {
     //admin for distribution services and lottery creation/deletion
     admin = msg.sender;
     lotteryPool = _lotteryPool;
   }
 
-  newLottery(uint _ticketsAvailable, uint _lotteryTime, uint _ticketPrice, address _lotteryPool) {
+  newLottery(uint _ticketsAvailable, uint _lotteryTime, uint _ticketPrice) {
 
     require(msg.sender == admin); // only the admin can create a new Lottery
     require(now >= (lotteryStart + lotteryTime)); // the previous lottery must have ended
@@ -33,7 +34,6 @@ contract Lottery {
     ticketsAvailable = _ticketsAvailable;
     lotteryTime      = _lotteryTime;
     ticketPrice      = _ticketPrice;
-    lotteryPool      = _lotteryPool;
 
     lotteryStart     = now;
 
@@ -60,9 +60,24 @@ contract Lottery {
     var purchaser = msg.sender;
 
     require(now <= (lotteryStart + lotteryTime)); // lottery isn't over
-    require(owner[ticket] == address(0));  // the ticket hasn't been purchased
-    require(msg.value == ticketPrice);     // value sent is the ticket price
+    require(owner[ticket] == address(0));         // the ticket hasn't been purchased
+    require(msg.value == ticketPrice);            // value sent is the ticket price
 
     owner[ticket] = purchaser;
+  }
+
+  completeLottery(uint winningHash) payable {
+    require(msg.sender == admin);
+
+    var winner = owner[winningHash % ticketsAvailable];
+
+    if (winner != address(0)) {
+      // TODO : decide what to do here, many options
+    }
+
+    LotteryCompleted(winner, this.balance);
+
+    winner.send(this.balance);
+    deleteLottery();
   }
 }
