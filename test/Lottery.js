c58628bf416e36c37ed21a76f58575d9d2796d68
Kyle Rose <kyle.rose@getg5.com>
2017-08-11 15:51:42 -0700

    deployment migration

--- test/Lottery.js
+++ test/Lottery.js
@@ -1,10 +1,11 @@
 require('babel-polyfill');
 
-var Lottery = artifacts.require('./Lottery.sol');
+var Lottery = artifacts.require('Lottery');
 
 
 contract('Lottery', function() {
   it("should deploy", async function() {
-    let meta = await Lottery.deployed();
+    let lotto = await Lottery.deployed();
+    assert.equal(1,1, "10000 wasn't in the first account");
   });
 });
